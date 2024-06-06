d36e45ec767d3cf65f3dc7e8a29b31722a66e577
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-06-06 11:08:51 +0530

    chore: Update Google Analytics configuration in scripts.tsx

--- packages/blog-starter-kit/themes/enterprise/components/scripts.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/scripts.tsx
@@ -3,10 +3,23 @@ export const Scripts = () => {
     window.dataLayer = window.dataLayer || [];
     function gtag(){window.dataLayer.push(arguments);}
     gtag('js', new Date());`;
+
+	// collate blog config
+	const googleAnalyticsConfig = `
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-NKRH7R2W0H');
+	`;
 	return (
 		<>
 			<script async src={`https://ping.hashnode.com/gtag/js?id=G-72XG3F8LNJ`} />
 			<script dangerouslySetInnerHTML={{ __html: googleAnalytics }} />
+
+			{/* collate blog config */}
+			<script async src={`https://www.googletagmanager.com/gtag/js?id=G-NKRH7R2W0H`}></script>
+			<script dangerouslySetInnerHTML={{ __html: googleAnalyticsConfig }} />
 		</>
 	);
 };
