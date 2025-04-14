5f4de758a2586258b136b427b76b73f930a0099a
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2025-04-14 17:22:32 +0530

    update gtag

--- packages/blog-starter-kit/themes/enterprise/components/scripts.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/scripts.tsx
@@ -1,10 +1,4 @@
 export const Scripts = () => {
-	const googleAnalytics = `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
-    gtag('js', new Date());`;
-
-	// collate blog config
 	const googleAnalyticsConfig = `
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
@@ -14,9 +8,6 @@ export const Scripts = () => {
 	`;
 	return (
 		<>
-			<script async src={`https://ping.hashnode.com/gtag/js?id=G-72XG3F8LNJ`} />
-			<script dangerouslySetInnerHTML={{ __html: googleAnalytics }} />
-
 			{/* collate blog config */}
 			<script async src={`https://www.googletagmanager.com/gtag/js?id=G-NKRH7R2W0H`}></script>
 			<script dangerouslySetInnerHTML={{ __html: googleAnalyticsConfig }} />
