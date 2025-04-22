df0821406ead1bc9fb1a4429c498557dfbde3604
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2025-04-22 15:55:48 +0530

    minor css updates

--- packages/blog-starter-kit/themes/enterprise/components/post-title.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/post-title.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const PostTitle = ({ children, className }: Props) => {
 	return (
 		<div
-			className={`prose-sm font-bold md:prose-lg dark:prose-invert prose-h1:text-center mx-auto max-w-screen-lg px-5 ${className}`}
+			className={`prose-sm font-bold md:prose-lg prose-h1:leading-[37.5px] md:prose-h1:leading-[60px] dark:prose-invert prose-h1:text-center mx-auto max-w-screen-lg px-5 ${className}`}
 		>
 			<h1 className="">{children}</h1>
 		</div>
--- packages/blog-starter-kit/themes/enterprise/pages/[slug].tsx
+++ packages/blog-starter-kit/themes/enterprise/pages/[slug].tsx
@@ -168,7 +168,7 @@ const Post = (publication: PublicationFragment, post: PostFullFragment) => {
 					/>
 					<MarkdownToHtml contentMarkdown={post.content.markdown} />
 					{(post.tags ?? []).length > 0 && (
-						<div className="text-slate-600 dark:text-neutral-300 lg:hidden">
+						<div className="text-slate-600 dark:text-neutral-300 px-5 my-3 lg:hidden">
 							<ul className="flex flex-row flex-wrap items-center gap-2">{tagsList}</ul>
 						</div>
 					)}
@@ -185,7 +185,7 @@ const Post = (publication: PublicationFragment, post: PostFullFragment) => {
 								size={8}
 								picture={post.author.profilePicture}
 							/>
-							<div className="mb-4 flex w-full flex-row flex-wrap  items-center gap-2 px-2 text-slate-700 dark:text-neutral-300 md:px-0">
+							<div className="mb-4 mt-2 flex w-full flex-row flex-wrap  items-center gap-2 px-2 text-slate-700 dark:text-neutral-300 md:px-0">
 								<DateFormatter dateString={post.publishedAt} />
 								{post.readTimeInMinutes && (
 									<span className="block font-bold text-slate-500">&middot;</span>
