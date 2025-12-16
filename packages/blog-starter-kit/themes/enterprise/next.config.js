406defe33f3388216b14bc7df62294118ae266cf
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2025-12-16 16:35:24 +0530

    Setup redirect to move to getcollate blog site (#32)
    * setup redirect
    
    * fix deployment error

--- packages/blog-starter-kit/themes/enterprise/next.config.js
+++ packages/blog-starter-kit/themes/enterprise/next.config.js
@@ -3,45 +3,54 @@ const ADVANCED_ANALYTICS_BASE_URL = 'https://stats.hashnode.com';
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const getBasePath = () => {
-	if (BASE_URL && BASE_URL.indexOf('/') !== -1) {
-		return BASE_URL.substring(BASE_URL.indexOf('/'));
-	}
-	return undefined;
+  if (BASE_URL && BASE_URL.indexOf('/') !== -1) {
+    return BASE_URL.substring(BASE_URL.indexOf('/'));
+  }
+  return undefined;
 };
 
 /**
  * @type {import('next').NextConfig}
  */
 const config = {
-	transpilePackages: ['@starter-kit/utils'],
-	basePath: getBasePath(),
-	experimental: {
-		scrollRestoration: true,
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'cdn.hashnode.com',
-			},
-		],
-	},
-	async rewrites() {
-		return [
-			{
-				source: '/ping/data-event',
-				destination: `${ANALYTICS_BASE_URL}/api/data-event`,
-			},
-			{
-				source: '/ping/view',
-				destination: `${ANALYTICS_BASE_URL}/api/view`,
-			},
-			{
-				source: '/api/collect',
-				destination: `${ADVANCED_ANALYTICS_BASE_URL}/api/collect`,
-			},
-		];
-	},
+  transpilePackages: ['@starter-kit/utils'],
+  basePath: getBasePath(),
+  experimental: {
+    scrollRestoration: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.hashnode.com',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ping/data-event',
+        destination: `${ANALYTICS_BASE_URL}/api/data-event`,
+      },
+      {
+        source: '/ping/view',
+        destination: `${ANALYTICS_BASE_URL}/api/view`,
+      },
+      {
+        source: '/api/collect',
+        destination: `${ADVANCED_ANALYTICS_BASE_URL}/api/collect`,
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.getcollate.io/blog/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = config;
